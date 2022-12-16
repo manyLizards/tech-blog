26f63ef578a581344fef55648fe88f5d6c05c94d
Jordan <jnbarringer96@gmail.com>
2022-12-16 16:36:37 -0600

    updated Post Model

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,5 @@
 //set up post model
+const { stubFalse } = require('lodash');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -6,6 +7,21 @@ class Post extends Model {}
 
 Post.init(
     {
-        
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        post_contents: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     }
 )
