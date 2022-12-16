84c2f44ed7ea8026d07fd13dcc3fa0ca3eb35dfe
Jordan <jnbarringer96@gmail.com>
2022-12-16 16:12:46 -0600

    set up handlebars as default template engine and started server to begin listening

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const path = require('path');
 //set up Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//set up handlebars.js as default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/post-routes'))
+
+//starts the server to begin listening
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
