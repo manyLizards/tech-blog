6b83cda74e791936e92d67d710c1840595d938c3
Jordan <jnbarringer96@gmail.com>
2022-12-20 15:55:30 -0600

    route to create/add post

--- controllers/post-routes.js
+++ controllers/post-routes.js
@@ -16,4 +16,18 @@ router.get('/post/:num', async (req, res) => {
     return res.render('post', posts[req.params.num - 1]);
 });
 
+//route to create/add a post
+router.post('/', async(req, res) => {
+    try {
+        const postData = await Post.create({
+            post_title: req.body.post_title,
+            post_date: req.body.post_date,
+            post_contents: req.body.post_contents
+        })
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
 module.exports = router;
